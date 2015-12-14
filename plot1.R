5b51344d6f6e17bdcd305c51199d93e071953894
yyulka <yyulka@gmail.com>
2015-12-13 16:19:21 -0800

    Added comments to code files

--- plot1.R
+++ plot1.R
@@ -2,6 +2,9 @@
 data_full <- read.table(unz("exdata-data-household_power_consumption.zip", 
 	"household_power_consumption.txt"), header = TRUE, sep = ";", 
 	stringsAsFactors = FALSE)
+# since the assignment instructions require to include only the code 
+# for reading the data I didn't include the code for downloading the zip archive
+
 data <- subset(data_full, data_full$Date %in% c("1/2/2007", "2/2/2007"))
 
 data$Global_active_power <- as.numeric(data$Global_active_power)
